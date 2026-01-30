c5b1adf3c0d4a31de53e679e62453bde4ed0fc75
MichaelPopov62 <romantikzizni@gmail.com>
2026-01-30 16:41:14 +0200

    made changes to the code structure

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import posthtml from 'posthtml';
-import include from 'posthtml-include';
 
 export default defineConfig({
-  root: 'src',  
-    server: {
-    host: '127.0.0.1',
-    port: 5173,
-    strictPort: true,
+  root: 'src',
+
+  publicDir: '../public',
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
   },
-  plugins: [
-    {
-      name: 'html-load',
-      enforce: 'pre',
-      transformIndexHtml(html) {
-        return posthtml([include()]).process(html).then(res => res.html);
-      },
-    },
-    createHtmlPlugin(),
-  ],
 });
